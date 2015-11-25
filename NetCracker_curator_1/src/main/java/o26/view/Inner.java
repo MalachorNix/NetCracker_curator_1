e6c6ed023dfdfc7e16e6738e3d72c89f26b643ba
Pavel Fadeev <malachornix@gmail.com>
2015-11-25 18:50:18 +0400

    Добавлена валидация ввода данных при добавлении задачи.

--- NetCracker_curator_1/src/main/java/o26/view/Inner.java
+++ NetCracker_curator_1/src/main/java/o26/view/Inner.java
@@ -7,28 +7,71 @@ import o26.model.TaskParameter;
 public class Inner {
 
     public Object doIt(TaskParameter parameter){
+
         String nameParameter = parameter.toString();
+        Scanner text = new Scanner(System.in);
+
         System.out.println("Введите "+nameParameter+":\n");
+
         switch(nameParameter){
-            case "DATE":{
+
+            case "DATE": {
                 Scanner in = new Scanner(System.in);
-                    System.out.print("Введите год: ");
-                    int year = in.nextInt();
-                    System.out.print("Введите месяц: ");
-                    int month = in.nextInt();
-                    System.out.print("Введите день: ");
-                    int day = in.nextInt();
-                    System.out.print("Введите час: ");
-                    int hour = in.nextInt();
-                    System.out.print("Введите минуту: ");
-                    int minute = in.nextInt();
-                    System.out.print("Введите секунду: ");
-                    int second= in.nextInt();
-                    return new GregorianCalendar(year, month-1, day, hour, minute, second);
+                System.out.print("Введите год: ");
+                int year = in.nextInt();
+                System.out.print("Введите месяц: ");
+                int month = in.nextInt();
+                System.out.print("Введите день: ");
+                int day = in.nextInt();
+                System.out.print("Введите час: ");
+                int hour = in.nextInt();
+                System.out.print("Введите минуту: ");
+                int minute = in.nextInt();
+                System.out.print("Введите секунду: ");
+                int second = in.nextInt();
+                return new GregorianCalendar(year, month - 1, day, hour, minute, second);
             }
-            default:{
-                return new Scanner(System.in).nextLine();
+
+            case "NAME": {
+
+                String name = text.nextLine();
+
+                while (name.length() == 0) {
+                    System.out.println("Пустое название запрещено.");
+                    System.out.println("Введите " + nameParameter + ":\n");
+                    name = text.nextLine();
+                }
+
+                return name;
             }
+
+            case "DESCRIPTION": {
+
+                String description = text.nextLine();
+
+                while (description.length() == 0) {
+                    System.out.println("Пустое описание запрещено.");
+                    System.out.println("Введите " + nameParameter + ":\n");
+                    description = text.nextLine();
+                }
+
+                return description;
+            }
+
+            case "CONTACTS": {
+
+                String contacts = text.nextLine();
+
+                while (contacts.length() == 0) {
+                    System.out.println("Пустое поле контактов запрещено.");
+                    System.out.println("Введите "+nameParameter+":\n");
+                    contacts = text.nextLine();
+                }
+
+                return contacts;
+            }
+            default:
+                return text.nextLine();
         }
     }
 }
