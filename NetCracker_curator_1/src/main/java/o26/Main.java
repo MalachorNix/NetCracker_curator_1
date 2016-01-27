e18878595351868a6a0ce1741461179960d43091
Pavel Fadeev <malachornix@gmail.com>
2016-01-27 13:31:37 +0400

    Добавлен экран входа пользователя в программу. Добавлено предложение выбора действия входа для пользователя.

--- NetCracker_curator_1/src/main/java/o26/Main.java
+++ NetCracker_curator_1/src/main/java/o26/Main.java
@@ -4,12 +4,9 @@ import o26.controller.*;
 import o26.view.MenuItem;
 import o26.view.MenuMenuItem;
 import o26.view.NotificationViewer;
+import o26.view.UserItem;
 
 public final class Main {
-
-    /*
-    * Сделал приватный конструктор и финальный класс, чтобы не ругался PMD.
-    * */
 
     private Main() {
 
@@ -19,20 +16,25 @@ public final class Main {
         Loader dataLoader = new DataLoader();
         ITaskCreator taskCreator = new TaskCreator();
         INotification notification = new Notification();
-            NotificationViewer notificationViewer = new NotificationViewer();
+        NotificationViewer notificationViewer = new NotificationViewer();
         notification.setNotificationViewer(notificationViewer);
-        MenuItem menuItem = new MenuMenuItem();
+        MenuItem menuItem = new MenuMenuItem(); //Итем главного меню
+        MenuItem userItem = new UserItem();
         Journal journal = new Journal();
 
         journal.setLoader(dataLoader);
         journal.setTaskCreator(taskCreator);
         journal.setNotification(notification);
+        journal.setView(userItem);
+
+        journal.showMenu();
+
         journal.setView(menuItem);
-        
+
         journal.load();
         journal.notificationStart();
         journal.showMenu();
-        
+
         System.exit(0);
     }
 }
--- NetCracker_curator_1/src/main/java/o26/view/UserItem.java
+++ NetCracker_curator_1/src/main/java/o26/view/UserItem.java
@@ -3,11 +3,58 @@ package o26.view;
 
 import o26.controller.Journal;
 
-public class UserItem extends MenuItem{
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class UserItem extends MenuItem {
+
+    private static final String ITEM = "Экран входа пользователя";
 
     @Override
     public void show(Journal journal) {
+        System.out.println("Добро пожаловать в планировщик задач!");
+        System.out.println("Чтобы войти в программу, нажмите 1.");
+        System.out.println("Чтобы зарегистрироваться, нажмите 2.");
+        System.out.println("Чтобы выйти из программы, нажмите 0.");
+
+        choice();
+
 
     }
 
+    private void choice() {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        try {
+            do {
+                System.out.print("Выберите действие: ");
+                choice = scanner.nextInt();
+                switch (choice) {
+                    case 1:
+                        System.out.println("Ввод логина/пароля");
+                        login();
+                        break;
+                    case 2:
+                        System.out.println("Регистрация");
+                        break;
+                    case 0:
+                        System.exit(0);
+                    default:
+                        System.out.println("Такого действия нет!");
+                }
+            } while (choice != 1);
+        } catch (InputMismatchException e) {
+            System.out.println("Вводите только целые числа.");
+            choice();
+        }
+    }
+
+    private void login() {
+
+    }
+
+    @Override
+    public String toString() {
+        return ITEM;
+    }
 }
